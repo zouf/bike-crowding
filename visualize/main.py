387da360b2f62c12de2a7d2d1610dbb23035c492
Matt Zoufaly <zouf@Matts-MacBook-Pro.local>
2023-12-09 20:37:06 -0500

    fix

--- visualize/main.py
+++ visualize/main.py
@@ -5,19 +5,12 @@ from google.cloud import storage
 import datetime
 import dateutil.parser
 import json
-from statistics import mean
 
 app = Flask(__name__)
 
 # Replace these values with your own
 BUCKET_NAME = "bike-crowding"
 DATA_FILE_NAME = "logs/central_park.csv"
-
-
-def _rolling_average(data):
-    print(vals)
-    return mean(vals)
-
 
 
 @app.route("/")
@@ -34,11 +27,20 @@ def plot_data():
     df = pd.read_csv('/tmp/data.csv', names=['timestamp','raw_count', 'location'])[['timestamp','raw_count']][-LIMIT:]
     df = df.assign(timestamp=pd.to_datetime(df['timestamp']), raw_count=pd.to_numeric(df['raw_count']))
     df  =df.set_index('timestamp')
+    avg_count = df['raw_count'].median()
+    max_count = df['raw_count'].max()
+    peak_time_utc = df[df['raw_count'] == max_count].index.values[0].replace(tzinfo=datetime.timezone.utc)
+    et_timezone = timezone(datetime.timedelta(hours=-4), 'America/New_York')
+
+
+    peak_time_et = peak_time_utc.astimezone(et_timezone)
+
+    
 
     dfs = df.resample("15min").mean().reset_index()
     dfs['timestamp'] = dfs.timestamp.map(lambda x: x.isoformat())
     data = dfs[['timestamp','raw_count']].to_dict('records')
-    return render_template("index.html", data=data)
+    return render_template("index.html", data=data, peak_time=peak_time_et, max_count=max_count, avg_count=avg_count)
 
 @app.route("/bike.js")
 def serve_bike_js():
