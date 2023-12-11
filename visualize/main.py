cfb6f0358d1990ed460918fb81b6aad0e6caa006
Matt Zoufaly <zouf@Matts-MacBook-Pro.local>
2023-12-10 20:43:10 -0500

    fix

--- visualize/main.py
+++ visualize/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, send_file, send_from_directory, request
+from wsgiref.util import request_uri
+from flask import Flask, render_template, send_file, send_from_directory, request, abort
 import numpy as np
 import pandas as pd
 from google.cloud import storage
@@ -10,6 +11,21 @@ app = Flask(__name__)
 # Replace these values with your own
 BUCKET_NAME = "bike-crowding"
 DATA_FILE_NAME = "logs/central_park.csv"
+
+@app.route("/raw/<path:path>")
+def serve_raw_file(path):
+    # Extract filename and date from path
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(BUCKET_NAME)
+    blob = bucket.blob(f"/home/mattzouf/bike-crowding/raw/{path}")
+    if blob.exists():
+        # Download and send the file
+        with open("/tmp/image.jpg", "wb") as f:
+            blob.download_to_file(f)
+        return send_file("/tmp/image.jpg", mimetype="image/jpg")
+    else:
+        # Return 404 Not Found
+        return abort(404)
 
 
 @app.route("/")
@@ -25,10 +41,11 @@ def plot_data():
         smoothing_minutes = int(request.args.get("smoothing_minutes"))
     except (TypeError, ValueError):
         smoothing_minutes = 60  # Default smoothing
-
-
+    smoothing_minutes = min(max(smoothing_minutes, 1), 360)
     window_size = max(window_size, 2)
+    date_range_max = dt.datetime.now().strftime('%Y%m%d')
     min_day = dt.datetime.today() - dt.timedelta(days=window_size)
+    date_range_min=min_day.strftime('%Y%m%d')
 
 
     # Download data from GCS
@@ -38,23 +55,34 @@ def plot_data():
     with open("/tmp/data.csv", "wb") as f:
         blob.download_to_file(f)
 
-    df = pd.read_csv('/tmp/data.csv', names=['timestamp','raw_count', 'location'])[['timestamp','raw_count']]
+    df = pd.read_csv('/tmp/data.csv', names=['timestamp','raw_count', 'location'])
     df = df.assign(timestamp=pd.to_datetime(df['timestamp']), raw_count=pd.to_numeric(df['raw_count']))
-
+    
     df = df[df['timestamp'] > min_day]
-
     df = df.set_index('timestamp')
     avg_count = np.round(df['raw_count'].median())
-    dfs = df.resample("60min").mean().reset_index()
+
+    dfs = df.resample(f"{smoothing_minutes}min").agg({'raw_count': np.mean, 'location': 'last'}).reset_index()
     max_count = dfs["raw_count"].fillna(0).max()
     peak_time_utc = pd.to_datetime(
         dfs[dfs["raw_count"] == max_count].timestamp.values[0], utc=True
     ).tz_convert("US/Eastern")
-
+    
     latest_count = np.round(dfs["raw_count"].values[-1])
-
     dfs["timestamp"] = dfs.timestamp.map(lambda x: x.isoformat())
-    data = dfs[["timestamp", "raw_count"]].to_dict("records")
+    dfs['raw_count'] = np.round(dfs['raw_count'])
+    
+    def fix_location(x):
+
+        rval = request.base_url+'raw'+x.split('raw')[1]
+        return rval
+
+
+    dfs['location'] = dfs['location'].map(fix_location)
+    data = dfs[["timestamp", "raw_count", "location"]].to_dict("records")
+
+    img_url = dfs['location'].values[-1]
+
     return render_template(
         "index.html",
         data=data,
@@ -63,6 +91,11 @@ def plot_data():
         avg_count=avg_count,
         latest_count=latest_count,
         window_size=window_size,
+        date_range_min=date_range_min,
+        date_range_max=date_range_max,
+        smoothing_minutes=smoothing_minutes,
+        img_url=img_url
+
     )
 
 @app.route("/bike.js")
--- visualize/static/bike.js
+++ visualize/static/bike.js
@@ -1,6 +1,5 @@
-const ctx = document.getElementById('timeSeriesChart').getContext('2d');
 
-// Define a function to format datetime with ET
+// Function to format datetime with ET
 function formatDateTime(datetimeStr) {
   const dateObj = new Date(datetimeStr);
   dateObj.setTime(dateObj.getTime() - dateObj.getTimezoneOffset() * 60 * 1000); // Convert from UTC to local time
@@ -14,83 +13,96 @@ function formatDateTime(datetimeStr) {
     second: "numeric",
     timeZone: "America/New_York", // Set time zone to ET
   };
-  return new Intl.DateTimeFormat("en-US", options).format(dateObj);
+  return dateObj; 
 }
 
-// Use the formatDateTime function to format labels
+// Format labels and data points
 const labels = bikeData.map(d => formatDateTime(d.timestamp));
 const dataPoints = bikeData.map(d => d.raw_count);
+const imgLocations = bikeData.map(d => d.location);
 
-const maxValue = Math.max(...dataPoints);
+
+// Calculate min and max values
 const minValue = Math.min(...dataPoints);
+const maxValue = Math.max(...dataPoints);
 
-// Define a color mapping function based on values
 function getColor(value) {
+  // Color mapping based on value
   const normalizedValue = (value - minValue) / (maxValue - minValue);
   const green = Math.round(255 * normalizedValue);
   const red = Math.round(255 * (1 - normalizedValue));
   return `rgba( ${green},${red}, 0, 0.5)`;
 }
 
-// Register Luxon as the date adapter
-Chart.register({
-  id: 'luxon',
-  beforeInit: function(chart) {
-    const { options } = chart;
-    if (options.scales.x.type === 'time' && options.scales.x.time && options.scales.x.time.parser) {
-      options.scales.x.time.parser = function(value) {
-        return DateTime.fromISO(value, { zone: 'utc' });
-      };
-    }
-  }
-});
-
-const chart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: labels,
-    datasets: [{
-      label: 'Entity Count',
-      data: dataPoints,
-      backgroundColor: dataPoints.map(getColor), // Use custom color mapping function
-      borderColor: 'rgba(0, 0, 0, 0.7)',
-      borderWidth: 1,
-      fill: false,
-      cubicInterpolationMode: 'monotone',
-      pointStyle: 'point',
-      tension: 0.4
-
-    }]
+// Define Plotly data and layout
+const data = [{
+  type: 'scatter',
+  mode: 'lines',
+  x: labels,
+  y: dataPoints,
+  line: {
+    // color: dataPoints.map(getColor),
+    width: 1,
+    shape: 'linear', // Change to 'linear' for a straight line
   },
-  options: {
-    responsive: true, // Disable responsiveness for maximizing size
+  fill: false,
+  hovertemplate: `Time: %{x}<br>Count: %{y}`, // Custom hover template
+  customdata: imgLocations, // Add image locations as custom data
 
-    plugins: {
-      title: {
-        display: true,
-        text: 'Count of People in Central Park'
-      },
-    },
+}];
 
-    width: window.innerWidth, // Set width to full window width
-    height: window.innerHeight * 0.8, // Set height to 80% of window height
-    tooltips: {
-      enabled: true,
-      mode: 'point',
-      callbacks: {
-        label: function(tooltipItem) {
-          // Format the tooltip label with additional information
-          return `Time: ${tooltipItem.xLabel}, Count: ${tooltipItem.yLabel}`;
-        }
-      }
-    },
-    
-    legend: {
-      display: false
-    },
+const layout = {
+  title: {
+    text: 'Count of People in Central Park',
+  },
+  xaxis: {
+    type: 'date',
+    tickformat: '%a, %Y-%m-%d %I:%M%p', 
+    tickangle: 10,
+    autorange: true,
     title: {
-      display: true,
-      text: 'Time Series Data'
+      text: 'Time'
     }
-  }
+  },
+  yaxis: {
+    title: {
+      text: 'Value'
+    },
+    range: [minValue - 0.1 * (maxValue - minValue), maxValue + 0.1 * (maxValue - minValue)], // Add some padding
+  },
+  width: window.innerWidth,
+  height: window.innerHeight * 0.8,
+  responsive: true,
+  legend: {
+    visible: false, // Hide legend by default
+  },
+};
+
+
+const timeSeriesChart = document.getElementById('timeSeriesChart');
+const imageContainer = document.getElementById('img-div'); // Create a div for the image
+imageContainer.id = 'imageContainer';
+imageContainer.style.display = 'inline-block'; // Display the image container inline
+imageContainer.style.verticalAlign = 'top'; // Align the container to the top
+
+Plotly.newPlot(timeSeriesChart, data, layout);
+
+// Add hover event listener
+timeSeriesChart.on('plotly_hover', function (event) {
+  const imageLocation = event.points[0].customdata;
+  // Make an HTTP GET request to the image location
+  fetch(imageLocation)
+    .then(response => response.blob())
+    .then(blob => {
+      // Display the image
+      const imageUrl = URL.createObjectURL(blob);
+      const imageElement = document.createElement('img');
+      imageElement.src = imageUrl;
+      // Clear previous image
+      while (imageContainer.firstChild) {
+        imageContainer.removeChild(imageContainer.firstChild);
+      }
+      // Append the new image to the container
+      imageContainer.appendChild(imageElement);
+    });
 });
