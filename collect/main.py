0839b2a5a03a693568cea561f5cab96f36941170
Matt Zoufaly <zouf@Matts-MacBook-Pro.local>
2024-11-23 11:13:00 -0500

    add app

--- collect/main.py
+++ collect/main.py
@@ -9,6 +9,8 @@ import os
 from pathlib import Path
 from flask import Flask
 
+app = Flask(__name__)
+
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
